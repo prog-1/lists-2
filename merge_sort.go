3bdb8ed1004137ac763ea97f6cbe184358b41eef
Aleksandr Stukalov <stukalovsaleksandrs@gmail.com>
2022-11-07 19:29:11 +0200

    Implemented Merge() undesirable way

--- merge_sort.go
+++ merge_sort.go
@@ -1,13 +1,33 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/prog-1/list"
 )
 
 // Merge combines two sorted lists into a single sorted list.
 func Merge(a, b *list.List) *list.List {
-	// TODO
-	return nil
+	var l list.List
+	curA, curB := a.Front(), b.Front()
+	var min *list.Element
+	for {
+		if curA != nil && curB != nil && curA.Value < curB.Value {
+			min = curA
+			curA = curA.Next()
+		} else if curB != nil {
+			min = curB
+			curB = curB.Next()
+		} else if curA != nil {
+			min = curA
+			curA = curA.Next()
+		} else {
+			break
+		}
+		l.PushBack(min.Value)
+	}
+
+	return &l
 }
 
 // Split splits a list into two lists with approximately the same length,
@@ -27,4 +47,14 @@ func MergeSort(l *list.List) *list.List {
 }
 
 func main() {
+	var a list.List
+	a.PushBack(1)
+	a.PushBack(3)
+	var b list.List
+	b.PushBack(2)
+	b.PushBack(4)
+	l := Merge(&a, &b)
+	for i := l.Front(); i != nil; i = i.Next() {
+		fmt.Println(i.Value)
+	}
 }
