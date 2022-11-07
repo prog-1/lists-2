585d923ad6dc266ae769317ca81d60224cee10fc
Aleksandr Stukalov <stukalovsaleksandrs@gmail.com>
2022-11-07 19:48:03 +0200

    Implemented Split() undesirable way

--- merge_sort.go
+++ merge_sort.go
@@ -36,8 +36,28 @@ func Split(l *list.List) (a, b *list.List) {
 	if l.Len() < 2 {
 		panic("length must be >= 2")
 	}
-	// TODO
-	return nil, nil
+
+	// Getting middle element:
+	var midEl *list.Element = l.Front()
+	mid := (int)(l.Len() / 2)
+	for i := 0; i < mid; i++ {
+		midEl = midEl.Next()
+	}
+
+	// Populating a:
+	a = &list.List{}
+	elA := l.Front()
+	for i := 0; i < mid; i, elA = i+1, elA.Next() {
+		a.PushBack(elA.Value)
+	}
+
+	// Populating b:
+	b = &list.List{}
+	for elB := elA; elB != nil; elB = elB.Next() {
+		b.PushBack(elB.Value)
+	}
+
+	return
 }
 
 // MergeSort sorts a list using the merge-sort algorithm.
@@ -47,14 +67,24 @@ func MergeSort(l *list.List) *list.List {
 }
 
 func main() {
-	var a list.List
-	a.PushBack(1)
-	a.PushBack(3)
-	var b list.List
-	b.PushBack(2)
-	b.PushBack(4)
-	l := Merge(&a, &b)
-	for i := l.Front(); i != nil; i = i.Next() {
+	var l list.List
+	l.PushBack(1)
+	l.PushBack(3)
+	l.PushBack(2)
+	l.PushBack(4)
+	l.PushBack(2)
+	l.PushBack(4)
+	//var b list.List
+	//b.PushBack(2)
+	//b.PushBack(4)
+	//l := Merge(&a, &b)
+	a, b := Split(&l)
+	for i := a.Front(); i != nil; i = i.Next() {
 		fmt.Println(i.Value)
 	}
+	fmt.Println("-----------------")
+	for i := b.Front(); i != nil; i = i.Next() {
+		fmt.Println(i.Value)
+	}
+
 }
